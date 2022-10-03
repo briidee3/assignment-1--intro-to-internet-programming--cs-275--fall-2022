e84c5d2666a00f7a08ed5784a706b8e9749f8f1d
Alec Dergachev <alecwithac27@gmail.com>
2022-10-03 12:33:09 -0400

    updated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,127 +1,19 @@
 const { watch } = require(`gulp`),
-    CSSLinter = require(`gulp-stylelint`),
-    del = require(`del`),
-    babel = require(`gulp-babel`),
-    htmlCompressor = require(`gulp-htmlmin`),
-    htmlValidator = require(`gulp-html`),
-    jsCompressor = require(`gulp-uglify`),
-    jsLinter = require(`gulp-eslint`),
-    sass = require(`gulp-sass`)(require(`sass`)),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload;
-
-let browserChoice = `default`;
-
-async function chrome () {
-    browserChoice = `google chrome`;
-}
-
-async function edge () {
-    // In Windows, the value might need to be “microsoft-edge”. Note the dash.
-    browserChoice = `microsoft-edge`;
-}
-
-async function firefox () {
-    browserChoice = `firefox`;
-}
-
-async function allBrowsers () {
-    browserChoice = [
-        `google chrome`,
-        `microsoft-edge`, // Note: In Windows, this might need to be microsoft-edge
-        `firefox`,
-    ];
-}
 
 let serve = () => {
     browserSync({
         notify: true,
         reloadDelay: 50,
-        browser: browserChoice,
         server: {
             baseDir: [
+            `dev/html`,
+            `dev`
             ]
         }
     });
-
     watch(`index.html`).on(`change`, reload);
 };
 
-async function clean() {
-    let fs = require(`fs`),
-        i,
-        foldersToDelete = [`./temp`, `prod`];
-
-    for (i = 0; i < foldersToDelete.length; i++) {
-        try {
-            fs.accessSync(foldersToDelete[i], fs.F_OK);
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory was found and will be deleted.\n`);
-            del(foldersToDelete[i]);
-        } catch (e) {
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory does NOT exist or is NOT accessible.\n`);
-        }
-    }
-
-    process.stdout.write(`\n`);
-}
-
-async function listTasks () {
-    let exec = require(`child_process`).exec;
-
-    exec(`gulp --tasks`, function (error, stdout, stderr) {
-        if (null !== error) {
-            process.stdout.write(`An error was likely generated when invoking ` +
-                `the “exec” program in the default task.`);
-        }
-
-        if (`` !== stderr) {
-            process.stdout.write(`Content has been written to the stderr stream ` +
-                `when invoking the “exec” program in the default task.`);
-        }
-
-        process.stdout.write(`\n\tThis default task does ` +
-            `nothing but generate this message. The ` +
-            `available tasks are:\n\n${stdout}`);
-    });
-}
-
-let lintCSS = () => {
-    return src(`dev/styles/css/**/*.css`)
-        .pipe(CSSLinter({
-            failAfterError: false,
-            reporters: [
-                {formatter: `string`, console: true}
-            ]
-        }));
-};
-
-exports.chrome = series(chrome, serve);
-exports.edge = series(edge, serve);
-exports.firefox = series(firefox, serve);
-exports.allBrowsers = series(allBrowsers, serve);
-exports.validateHTML = validateHTML;
-exports.lintJS = lintJS;
-exports.transpileJSForDev = transpileJSForDev;
-exports.compressHTML = compressHTML;
-exports.compileCSSForProd = compileCSSForProd;
-exports.transpileJSForProd = transpileJSForProd;
-exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
-exports.clean = clean;
-exports.default = listTasks;
-exports.lintCSS = lintCSS;
-exports.serve = series(
-    validateHTML,
-    compileCSSForDev,
-    lintJS,
-    transpileJSForDev,
-    serve
-);
-exports.build = series(
-    compressHTML,
-    compileCSSForProd,
-    transpileJSForProd,
-    compressImages,
-    copyUnprocessedAssetsForProd
-);
+exports.serve = series;
