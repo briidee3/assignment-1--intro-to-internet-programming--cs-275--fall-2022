cda4798372d2fa8e6c0e444a9b0f3edb38d9e070
Alec Dergachev <alecwithac27@gmail.com>
2022-10-05 12:36:28 -0400

    added code for array

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,13 +1,28 @@
 window.onload = () => {
-    let main = document.queryCommandIndeterm(`main`);
-    let myArray = new Array(20);
-    main.innerHTML = `<p>The size of my array is ${myArray.length}.</p>`;
-};
+    let output = document.getElementById(`output`);
+    let content = ``;
+    let counter = 1;
 
-let input = window.prompt(`Insert a number here`);
+    let input = window.prompt(`Insert a number here`);
+    input = parseInt(input, 10);
+    console.log(typeof input);
+    if (input < 2) {
+        alert(`Integers less than 2 are not allowed. Please try again.`)
+    } else if (isNaN(input) === true) {
+        alert(`Input invalid. Please try again.`)
+    }
 
-if (input < 2) {
-    alert(`Integers less than 2 are not allowed. Please try again.`)
-} else if (isNaN(input) == true) {
-    alert(`Input invalid. Please try again.`)
+    let myNewArray = new Array(input);
+    console.log(`the size of my new array is ${myNewArray.length}`);
+
+    content = `<table>`;
+    for (let i = 0; i < myNewArray.length; i++) {
+        content += `<tr>`;
+        for (let j = 0; j < myNewArray.length; j++) {
+            content += `<td>${counter++}</td>`;            
+        }
+        content += '</tr>'
+    }
+    content += `</table>`;
+    output.innerHTML = content;
 }
