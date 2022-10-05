a7f4b03cdfcaa0d318bb1ee8ab442e64f8ce8329
Alec Dergachev <alecwithac27@gmail.com>
2022-10-05 12:02:14 -0400

    Finalized conditions

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,9 +1,13 @@
 window.onload = () => {
-    alert(`JavaScript Works`);
+    let main = document.queryCommandIndeterm(`main`);
+    let myArray = new Array(20);
+    main.innerHTML = `<p>The size of my array is ${myArray.length}.</p>`;
 };
 
 let input = window.prompt(`Insert a number here`);
 
 if (input < 2) {
     alert(`Integers less than 2 are not allowed. Please try again.`)
+} else if (isNaN(input) == true) {
+    alert(`Input invalid. Please try again.`)
 }
