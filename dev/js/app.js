50fd0eb276f08779ec94bc32a364c097b419a83d
TazK7 <khantazz07@gmail.com>
2022-10-05 12:34:37 -0400

    Added JS code
    Added first lines of JS code for user input for the matrix.

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,4 +2,16 @@ window.onload = () => {
     alert(`JavaScript Works`);
 };
 
-let input =  window.prompt("Insert a Number Here");
+let input =  window.prompt(`Insert a Number Here`);
+input = parseInt(input, 10);
+console.log(typeof input);
+
+let newArray = new Array(input);
+console.log(`This is the size of your array: ${newArray.length}`)
+
+    content = `<table>`;
+
+    for(let i = 0; i < newArray.length; i++) {
+        content += '<tr>';
+
+    }
