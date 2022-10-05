c735143104de68445d15f387caeae10cd43084b5
Basmalahasad <asad@hartford.edu>
2022-10-05 00:30:50 -0400

    Function to check if input is valid

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,12 @@
-window.onload = () => {
-    alert(`JavaScript Works`);
-};
+let enteredInteger = prompt(`What size is your square matrix?`);
+let afterParse = parseInt(parseFloat(enteredInteger));
+
+if (isNaN(afterParse)){
+    alert(`The input is invalid`);
+}
+else if (afterParse <= 0){
+    alert(`The input is less than 1`);
+}
+else if (Number.isInteger(afterParse)){
+    alert(`The input is not an integer`);
+}
---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+
+const { src, dest, series, watch } = require(`gulp`),
+    babel = require(`gulp-babel`),
+    htmlValidator = require(`gulp-htmlmin`),
+    cssValidator = require(`gulp-clean-css`),
+    jsCompressor = require(`gulp-uglify`),
+    jsValidator = require(`gulp-eslint`),
+    browserSync = require(`browser-sync`)
+    reload = browserSync.reload;
+
+let browserChoice = `default`;
+
+let compressCSS = () => {
+    return src([`dev/css/*.css`,`dev/css/**/*.css`])
+        .pipe(htmlValidator({collapseWhitespace: true}))
+        .pipe(dest(`prod/css`));
+};
+
+let validateCSS = () => {
+    return src([
+        `dev/css/*.css`,
+        `dev/css/**/*.css`])
+        .pipe(cssValidator(undefined));
+}
+
+let validateHTML = () => {
+    return src([
+        `dev/html/*.html`,
+        `dev/html/**/*.html`])
+        .pipe(htmlValidator(undefined));
+};
+
+let validateJS = () => {
+    return src([
+        `dev/js/*.js`,
+        `dev/js/**/*.js`])
+        .pipe(jsValidator())
+        .pipe(jsValidator.formatEach(`compact`));
+};
+
+let compileCSSForDev = () => {
+    return src(`dev/styles/scss/main.scss`)
+        .pipe(sass.sync({
+            outputStyle: `expanded`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`temp/styles`));
+};
+
+let transpileJSForDev = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let compressHTML = () => {
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `dev`,
+                `dev/html`
+            ]
+        }
+    });
+
+    watch(`dev/html/*.html`, validateHTML).on(`change`, reload);
+    watch(`dev/css/*.css`, validateCSS).on(`change`, reload);
+    watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
+};
+
+
+
+exports.validateHTML = validateHTML;
+exports.validateCSS = validateCSS;
+exports.validateJS = validateJS;
+exports.compileCSSForDev = compileCSSForDev;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
+exports.transpileJSForProd = transpileJSForProd;
+exports.serve = series(
+    validateHTML,
+    validateCSS,
+    validateJS,
+    transpileJSForDev,
+    serve
+);
