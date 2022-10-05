d43b4bbba67b52302a6a1c2ccd0349c6e21520f3
bri <briidee3@gmail.com>
2022-10-05 15:18:05 -0400

    Set up base array in javascript

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,27 @@
 window.onload = () => {
     
     let output = document.getElementById(`output`);
+    let content = ``;
+
+    let input = parseInt(window.prompt(`What size should the matrix be?`));
+
+    let mainArray = new Array(input);
+    console.log(`Array size is ${input.length}`);
+
+    content = `<table>`;
+
+    for (let i = 0; i < mainArray.length; i++) {
+        content += `<tr>`;
+
+        for (let j = 0; j < mainArray.length; j++) {
+            content += `<td>${1 + j + parseInt(i*mainArray.length)}</td>`;
+        }
+
+        content += `</tr>`;
+    }
+
+    content += `</table>`;
+
+    output.innerHTML = content;
 
 };
