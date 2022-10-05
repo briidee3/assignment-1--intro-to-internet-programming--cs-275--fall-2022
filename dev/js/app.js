c99aef18d45efff288690978ba8a05cc3b687c31
bri <briidee3@gmail.com>
2022-10-05 16:08:20 -0400

    flipped the matrix

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,7 +1,8 @@
 window.onload = () => {
     
     // set up initial variables
-    let matrix = document.getElementById(`initial`);
+    let initial = document.getElementById(`initial`);
+    let final = document.getElementById(`final`);
     let content = ``;
 
     let input = parseInt(window.prompt(`What size should the matrix be?`));
@@ -9,15 +10,17 @@ window.onload = () => {
     let mainArray = new Array(input);
     console.log(`Array size is ${input.length}`);
 
+    let len = mainArray.length;     // ease of use with calculations
+
 
     // display initial matrix
     content = `<table>`;
 
-    for (let i = 0; i < mainArray.length; i++) {
+    for (let i = 0; i < len; i++) {
         content += `<tr>`;
 
-        for (let j = 0; j < mainArray.length; j++) {
-            content += `<td>${1 + j + parseInt(i*mainArray.length)}</td>`;
+        for (let j = 0; j < len; j++) {
+            content += `<td>${1 + j + (i*len)}</td>`;
         }
 
         content += `</tr>`;
@@ -25,9 +28,31 @@ window.onload = () => {
 
     content += `</table>`;
 
-    matrix.innerHTML = content;
+    initial.innerHTML = content;
 
 
-    
+    // display final (flipped) matrix
+    content = `<table>`; 
+
+    for (let i = 0; i < len; i++) {
+        content += `<tr>`;
+
+        for (let j = 0; j < len; j++) {
+
+            let currNum = j + (i*len);  // set as default value for now
+
+            if (!((len + ((len - 1) * i)))) {   // so the ones from northeast to southwest corners aren't affected
+                
+            }
+            currNum = len*len - currNum;
+            content += `<td>${currNum}</td>`;
+        }
+
+        content += `</tr>`;
+    }
+
+    content += `</table>`;
+
+    final.innerHTML = content;
 
 };
