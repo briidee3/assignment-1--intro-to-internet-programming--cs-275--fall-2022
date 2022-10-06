6309edf9a6050fa8c1da5be97d31a980e66283c6
TazK7 <khantazz07@gmail.com>
2022-10-06 18:59:21 -0400

    Added all the Functions to get gulp running

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,54 @@
-const { watch } = require(`gulp`),
-CSSLinter = require(`gulp-stylelint`),
-    del = require(`del`),
+const { watch, src, dest, series } = require(`gulp`),
     babel = require(`gulp-babel`),
-    htmlCompressor = require(`gulp-htmlmin`),
-    htmlValidator = require(`gulp-html`),
+    htmlValidator = require(`gulp-htmlmin`),
     jsCompressor = require(`gulp-uglify`),
-    jsLinter = require(`gulp-eslint`),
+    jsValidator = require(`gulp-eslint`),
     browserSync = require(`browser-sync`),
+    cssValidator = require(`gulp-clean-css`),
     reload = browserSync.reload;
+
+    
+
+let validateHTML = () => {
+    return src(`dev/html/index.html`)
+        .pipe(cssValidator(undefined));
+}
+
+let validateCSS = () => {
+    return src(`dev/css/style.css`)
+        .pipe(htmlValidator(undefined));
+}
+
+let compressHTML = () => {
+    return src(`dev/html/index.html`)
+        pipe(htmlCompressor({collapseWhitespace: true}))
+}
+
+let compressCSS = () => {
+    return src(`dev/css/style.css`)
+        .pipe(jsCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+}
+
+
+let validateJS = () => {
+    return src(`dev/js/app.js`)
+        .pipe(jsValidator())
+        .pipe(dest(`temp/scripts`));
+}
+
+let transpileJSForDev = () => {
+    return src(`dev/js/app.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+}
+
+let transpileJSForProd = () => {
+    return src(`dev/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
 
 let serve = () => {
     browserSync({
@@ -15,6 +56,7 @@ let serve = () => {
         reloadDelay: 50,
         server: {
             baseDir: [
+                `temp`,
                 `dev/html`,
                 `dev`
             ]
@@ -27,3 +69,16 @@ let serve = () => {
 };
 
 exports.default = serve;
+exports.validateCSS = validateCSS;
+exports.validateJS = validateJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS; 
+exports.transpileJSForProd = transpileJSForProd;
+exports.serve = serve; 
+exports.build = series(
+    validateHTML,
+    validateJS,
+    transpileJSForDev,
+    serve
+)
