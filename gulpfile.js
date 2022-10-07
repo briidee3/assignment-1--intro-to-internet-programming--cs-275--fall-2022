d4adaba62e282326035bbfae01dd75a8d10f7fa3
Alec Dergachev <alecwithac27@gmail.com>
2022-10-07 14:20:51 -0400

    Adding necessary gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,35 @@
 const { watch } = require(`gulp`),
+    babel = require(`gulp-babel`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload;
+
+let transpileJSForDev = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+let lintJS = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let compressHTML = () => {
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
 
 let serve = () => {
     browserSync({
@@ -19,4 +48,8 @@ let serve = () => {
     watch(`index.html`).on(`change`, reload);
 };
 
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.compressHTML = compressHTML;
+exports.lintJS = lintJS;
 exports.serve = serve;
