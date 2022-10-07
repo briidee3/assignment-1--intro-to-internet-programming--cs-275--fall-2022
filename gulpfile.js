1bd49d602e729169e814b98659ce26e48278b43e
Alec Dergachev <alecwithac27@gmail.com>
2022-10-07 15:43:39 -0400

    Finalized gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,48 @@
-const { watch } = require(`gulp`),
-    babel = require(`gulp-babel`),
+const { src, dest, series, watch } = require(`gulp`),
     htmlCompressor = require(`gulp-htmlmin`),
+    cssCompressor = require(`gulp-clean-css`),
+    jsValidator = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
     jsCompressor = require(`gulp-uglify`),
-    jsLinter = require(`gulp-eslint`),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload;
 
+    let compressHTML = () => {
+    return src(`dev/html/index.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compressCSS = () => {
+    return src(`dev/css/style.css`)
+        .pipe(cssCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compressJS = () => {
+    return src(`dev/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod`));
+};
+
+let validateJS = () => {
+    return src(`dev/js/app.js`)
+    .pipe(jsValidator())
+    .pipe(jsValidator.formatEach(`compact`, process.stderr));
+};
+
 let transpileJSForDev = () => {
-    return src(`dev/scripts/*.js`)
+    return src(`dev/js/app.js`)
         .pipe(babel())
         .pipe(dest(`temp/scripts`));
 };
 
 let transpileJSForProd = () => {
-    return src(`dev/scripts/*.js`)
+    return src(`dev/js/app.js`)
         .pipe(babel())
         .pipe(jsCompressor())
         .pipe(dest(`prod/scripts`));
-};
-
-let lintJS = () => {
-    return src(`dev/scripts/*.js`)
-        .pipe(jsLinter())
-        .pipe(jsLinter.formatEach(`compact`));
-};
-
-let compressHTML = () => {
-    return src([`dev/html/*.html`,`dev/html/**/*.html`])
-        .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`prod`));
 };
 
 let serve = () => {
@@ -37,19 +51,30 @@ let serve = () => {
         reloadDelay: 50,
         server: {
             baseDir: [
-            `dev/html`,
-            `dev`
+                `dev`,
+                `dev/html`
             ]
         }
     });
 
-    watch(`js/app.js`).on(`change`, reload);
-    watch(`css/style.css`).on(`change`, reload);
-    watch(`index.html`).on(`change`, reload);
+    watch(`dev/html/*.html`).on(`change`, reload);
+    watch(`dev/css/*.css`).on(`change`, reload);
+    watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
+
 };
 
+
+
+exports.validateJS = validateJS;
 exports.transpileJSForDev = transpileJSForDev;
-exports.transpileJSForProd = transpileJSForProd;
 exports.compressHTML = compressHTML;
-exports.lintJS = lintJS;
+exports.compressCSS = compressCSS;
+exports.compressJS = compressJS;
+exports.transpileJSForProd = transpileJSForProd;
 exports.serve = serve;
+exports.build = series(
+    compressJS,
+    transpileJSForProd,
+    compressHTML,
+    compressCSS,
+);
