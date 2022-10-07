520de9e76507213ae19dab2ed5fa636b6fc3b22d
aidangraff <graff@hartford.edu>
2022-10-07 14:33:45 -0400

    fixed some gulp modules and added some html

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
 const { src, dest, series, watch } = require(`gulp`),
-    htmlValidator = require(`gulp-htmlmin`),
-    cssValidator = require(`gulp-clean-css`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    cssCompressor = require(`gulp-clean-css`),
     jsValidator = require(`gulp-eslint`),
     babel = require(`gulp-babel`),
     jsCompressor = require(`gulp-uglify`),
     browserSync = require(`browser-sync`),
     reload = browserSync.reload;
 
-let validateHTML = () => {
-    return src(`dev/html/index.html`)
-        .pipe(htmlValidator(undefined));
-}
-
-let validateCSS = () => {
-    return src(`dev/css/style.css`)
-        .pipe(cssValidator(undefined));
-}
 
 let compressHTML = () => {
     return src(`dev/html/index.html`)
@@ -25,14 +16,21 @@ let compressHTML = () => {
 
 let compressCSS = () => {
     return src(`dev/css/style.css`)
-        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(cssCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compressJS = () => {
+    return src(`dev/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
         .pipe(dest(`prod`));
 };
 
 let validateJS = () => {
     return src(`dev/js/app.js`)
-        .pipe(jsValidator())
-        .pipe(jsValidator.formatEach(`compact`));
+    .pipe(jsValidator())
+    .pipe(jsValidator.formatEach(`compact`, process.stderr));
 };
 
 let transpileJSForDev = () => {
@@ -54,29 +52,30 @@ let serve = () => {
         reloadDelay: 50,
         server: {
             baseDir: [
-                `temp`,
                 `dev`,
                 `dev/html`
             ]
         }
     });
 
-    watch(`dev/html/*.html`, validateHTML).on(`change`, reload);
-    watch(`dev/css/*.css`, validateCSS).on(`change`, reload);
+    watch(`dev/html/*.html`).on(`change`, reload);
+    watch(`dev/css/*.css`).on(`change`, reload);
     watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
 
 };
 
 
-exports.validateHTML = validateHTML;
+
 exports.validateJS = validateJS;
 exports.transpileJSForDev = transpileJSForDev;
 exports.compressHTML = compressHTML;
 exports.compressCSS = compressCSS;
+exports.compressJS = compressJS;
 exports.transpileJSForProd = transpileJSForProd;
+exports.serve = serve;
 exports.build = series(
-    validateHTML,
-    validateJS,
-    transpileJSForDev,
-    serve
+    compressJS,
+    transpileJSForProd,
+    compressHTML,
+    compressCSS,
 );
