89b91b8f9fe8e30b79188737c580db0276764b5f
TazK7 <khantazz07@gmail.com>
2022-10-07 15:24:43 -0400

    Trying to get gulp build working

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,40 @@
 const { watch, src, dest, series } = require(`gulp`),
     babel = require(`gulp-babel`),
-    htmlValidator = require(`gulp-htmlmin`),
+    htmlCompressor = require(`gulp-htmlmin`),
     jsCompressor = require(`gulp-uglify`),
     jsValidator = require(`gulp-eslint`),
     browserSync = require(`browser-sync`),
-    cssValidator = require(`gulp-clean-css`),
+    cssCompressor = require(`gulp-clean-css`),
     reload = browserSync.reload;
-
-let validateHTML = () => {
-    return src(`dev/html/index.html`)
-        .pipe(cssValidator(undefined));
-}
-
-let validateCSS = () => {
-    return src(`dev/css/style.css`)
-        .pipe(htmlValidator(undefined));
-}
 
 let compressHTML = () => {
     return src(`dev/html/index.html`)
-        pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(htmlCompressor({collpaseWhitespace: true}))
+        .pipe(dest(`prod`));
 }
 
 let compressCSS = () => {
     return src(`dev/css/style.css`)
-        .pipe(jsCompressor({collapseWhitespace: true}))
+        .pipe(cssCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
+}
+
+let validateHTML = () => {
+    return src(`dev/html/index.html`)
+        .pipe(htmlValidator(undefined));
+};
+
+let validateCSS = () => {
+    return src(`dev/css/style.css`)
+        .pipe(cssValidator(undefined));
 }
 
 
 let validateJS = () => {
     return src(`dev/js/app.js`)
-        .pipe(jsValidator())
-        .pipe(dest(`temp/scripts`));
-}
+    .pipe(jsValidator())
+    .pipe(jsValidator.formatEach(`compact`));
+};
 
 let transpileJSForDev = () => {
     return src(`dev/js/app.js`)
@@ -66,17 +67,18 @@ let serve = () => {
     watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
 };
 
-exports.default = serve;
+exports.validateHTML = validateHTML;
 exports.validateCSS = validateCSS;
 exports.validateJS = validateJS;
-exports.transpileJSForDev = transpileJSForDev;
 exports.compressHTML = compressHTML;
-exports.compressCSS = compressCSS; 
+exports.compressCSS = compressCSS;
+exports.transpileJSForDev = transpileJSForDev;
 exports.transpileJSForProd = transpileJSForProd;
-exports.serve = serve; 
+exports.serve = serve;
 exports.build = series(
     validateHTML,
-    validateJS,
-    transpileJSForDev,
-    serve
-)
+    validateCSS,
+    compressHTML,
+    compressCSS,
+    transpileJSForProd,
+);
