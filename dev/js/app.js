613d5d5101f843374fa2c9170ec26c05c7a5f979
Alec Dergachev <alecwithac27@gmail.com>
2022-10-07 14:34:33 -0400

    Added redo task

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,19 +1,34 @@
 window.onload = () => {
+    let retry = false;
     let output = document.getElementById(`output`);
     let reverse = document.getElementById(`reverse`);
     let content = ``;
     let counter = 1;
 
     let input = window.prompt(`Insert a number here`);
-    input = parseInt(input, 10);
-    console.log(typeof input);
+    let parsed = parseInt(parseFloat(input));
     if (input < 2) {
-        alert(`Integers less than 2 are not allowed. Please try again.`)
-    } else if (isNaN(input) === true) {
-        alert(`Input invalid. Please try again.`)
+        alert(`Integers less than 2 are not allowed. Please try again.`);
+        retry = true;
+    } else if (isNaN(parsed) === true) {
+        alert(`Input invalid. Please try again.`);
+        retry = true;
     }
 
-    let myArray = new Array(input);
+    while (retry) {
+        input = window.prompt(`Insert a number here`);
+        parsed = parseInt(parseFloat(input));
+        if (input < 2) {
+            alert(`Integers less than 2 are not allowed. Please try again.`);
+            retry = true;
+        } else if (isNaN(parsed) === true) {
+            alert(`Input invalid. Please try again.`);
+            retry = true;
+        } else {
+            retry = false;
+        }
+    }
+    let myArray = new Array(parsed);
     console.log(`the size of my new array is ${myArray.length}`);
     let revcounter = Math.pow(myArray.length, 2);
 
