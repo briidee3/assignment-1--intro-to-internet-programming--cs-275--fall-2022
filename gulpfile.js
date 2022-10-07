500fde140d08c8a485f7199365a19d111a2f54d6
Matthew Sirois <mjsirois2@gmail.com>
2022-10-07 01:03:54 -0400

    Implement gulp modules and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, series, watch } = require(`gulp`),
     htmlCompressor = require(`gulp-htmlmin`),
-    cleanCSS = require('gulp-clean-css');
+    validateCSS = require('gulp-clean-css');
     jsLinter = require(`gulp-eslint`),
     babel = require(`gulp-babel`),
     jsCompressor = require(`gulp-uglify`),
@@ -13,33 +13,55 @@ let validateHTML = () => {
     return src([
         `dev/html/*.html`,
         `dev/html/**/*.html`])
-        .pipe(htmlValidator(undefined));
+        .pipe(htmlCompressor(undefined));
 };
 
 let compressHTML = () => {
-    return src([`dev/html/*.html`])
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
 };
 
+let compressCSS = () => {
+    return src([`dev/css/*.css`,`dev/css/**/*.css`])
+        .pipe(validateCSS({collapseWhitespace: true}))
+        .pipe(dest(`prod/css`));
+}
 
-let compressCSS = () => {};
-let validateJS = () => {};
-let transpileJSForDev = () => {};
-let transpileJSForProd = () => {};
+let validateJS = () => {
+    return src([`dev/js/*.js`, `dev/js/**/*.js`])
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
 
 let serve = () => {
     browserSync({
         notify: true,
         reloadDelay: 50,
+        browser: browserChoice,
         server: {
-            baseDir: `./`
+            baseDir: [
+                `temp`,
+                `dev`,
+                `dev/html`
+            ]
         }
     });
 
     watch(`dev/html/*.html`, validateHTML).on(`change`, reload);
     watch(`dev/css/*.css`, validateCSS).on(`change`, reload);
     watch(`dev/js/*.js`, series(validateJS, transpileJSForDev)).on(`change`, reload);
-};
-
-exports.default = serve;
+}
