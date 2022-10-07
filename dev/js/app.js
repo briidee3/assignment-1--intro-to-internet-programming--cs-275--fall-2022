ce58714ac1b3ab4dee482caadb637589a9f23a8d
Camren13-21 <caferguso@hartford.edu>
2022-10-07 15:26:05 -0400

    flipped matrix project
    program is suppose to highlight numbers in table and flip those numbers in the next matrix.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,11 @@
 window.onload = () => {
     alert(`JavaScript Works`);
 };
+let compressHTML = () => {};
+
+let compressCSS = () => {};
+let validateJS = () => {};
+let transpileJSForDev = () => {};
+let transpileJSForProd = () => {};
+let serve = () => {};
+
