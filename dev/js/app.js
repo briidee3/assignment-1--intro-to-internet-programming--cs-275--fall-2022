cd806451ae665ba9c035425b68b2f678a7b6ad74
Matthew Sirois <mjsirois2@gmail.com>
2022-12-09 10:38:54 -0500

    Readd Assignment 1

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,66 +1,72 @@
 window.onload = () => {
-    let firstArray = document.getElementById(`firstArray`);
-    let flippedArray = document.getElementById(`flippedArray`);
+    let firstArray = document.getElementById(`InputArray`);
+    let ReversedArray = document.getElementById(`ReversedArray`);
     let content = ``;
-    let counter = 1;
+    let index = 1;
+
     // Input prompt to the user
     let input = window.prompt(`What is the size of the matrix?`);
-    // Sentinel
+
+    // Sentinel for only allowing integer values larger than 1
     while (input < 2) {
         input = window.prompt(`Incorrect input, please enter a whole number larger than 1.`);
     }
-    //Assign user input value to array value
+
+    //Verify an integer input
     input = parseInt(input);
-    console.log(typeof input);
+
+    //Initialize input array
     let inputArray = new Array(input);
 
+    // Generating the input matrix, where index is the current location in the table
+    // Each for loop will populate each respective row and cell for each column
     content = `<table>`;
-    // Generating normal array
     for(let i = 0; i < inputArray.length; i++) {
         content += `<tr>`;
 
         for(let j = 0; j < inputArray.length; j++) {
-            content += `<td>${counter++}</td>`;
+            content += `<td>${index++}</td>`;
         }
 
         content += `</tr>`;
     }
 
     content += `</table>`;
-
+    //Send out content to the webpage under h2 header
     firstArray.innerHTML = content;
 
-    //Flipped matrix
+    //Transforming the input array into a 2D array to be prepared for flipping each cell
+    let counter = 1;
+    for (let i = 0; i < input; i++) {
+        inputArray[i] = [];
+
+        for (let j = 0; j < input; j++) {
+            inputArray[i][j] = counter;
+        }
+    }
+
+    //Flipping the matrix
 
     content = `<table>`;
-
+    //Outer for loop for populating each respective row in the matrix
     for(let i = 0; i < inputArray.length; i++) {
         content += `<tr>`;
-
+        //Inner for loop for each cell in columns
+        //Using a temp variable to swap the indexes, and pass over the minor diagonal in the matrix
         for(let j = 0; j < inputArray.length; j++) {
-            //Holding a temporary variable for current place in array
-            let tempHolder = (i * inputArray.length) + j + 1;
-            //Checking for any potential diagonal values in array
-            if(!((inputArray.length + ((inputArray.length - 1) * i) === tempHolder ))) {
+            let temp = inputArray[i][j];
+            inputArray[i][j] = inputArray[(input - 1) - j][(input - 1) - i];
+            temp = inputArray[(input - 1) - j][(input - 1) - i];
 
-                tempHolder = (inputArray.length * inputArray.length) - tempHolder;
-                tempHolder++;
-            }
-
-            content += `<td>${tempHolder}</td>`;
-
+            content += `<td>${temp}</td>`;
         }
 
         content += `</tr>`;
+
     }
 
     content += `</table>`;
-
-    flippedArray.innerHTML = content;
-
-
-
-
-
+    //Send out content to webpage under the h2 header for flipped array
+    ReversedArray.innerHTML = content;
 
 };
